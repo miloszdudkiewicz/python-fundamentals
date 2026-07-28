fd57fde6c41a76a7b84c17d42b9ba9b6a2469fd8
miloszdudkiewicz <milosz.dudkiewiczb@gmail.com>
2026-07-28 12:43:40 +0200

    Deleted result and print(result)

--- basics/functions.py
+++ basics/functions.py
@@ -2,5 +2,3 @@ def calculate_average(numbers):
     if not numbers:
         raise ValueError("List cannot be empty")
     return sum(numbers) / len(numbers)
-result = calculate_average([30,40,50])
-print (result)
