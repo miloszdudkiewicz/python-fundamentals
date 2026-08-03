22e8949b73c16fef220eccf2e1da714282293c85
miloszdudkiewicz <milosz.dudkiewiczb@gmail.com>
2026-08-03 15:29:25 +0200

    Added count_occurrences function

--- basics/functions.py
+++ basics/functions.py
@@ -16,4 +16,56 @@ def find_max(numbers):
     return current_max
 
 
+def find_min(numbers):
+    if not numbers:
+        raise ValueError("The list cannot be empty")
     
+    current_min = numbers[0]
+
+    for number in numbers:
+        if current_min > number:
+            current_min = number
+
+    return current_min
+
+
+def find_sum(numbers):
+    if not numbers:
+        raise ValueError ("To find the sum, the list must contain numbers")
+    
+    current_sum = 0
+
+    for number in numbers:
+        current_sum += number
+
+    return current_sum
+
+def find_second_highest(numbers):
+    if len(numbers) < 2:
+        raise ValueError("" \
+        "To find the second highest, the list must contain at least 2 values"
+        )
+
+    highest = numbers[0]
+    second_highest = numbers[1]
+
+    if highest < second_highest:
+        highest, second_highest = second_highest, highest
+
+    for number in numbers[2:]:
+        if number > highest:
+            highest, second_highest = number, highest
+        elif number < highest and number > second_highest:
+            second_highest = number
+
+    return second_highest
+
+def count_occurrences(numbers, to_count):
+
+    counter = 0
+    for number in numbers:
+        if number == to_count:
+            counter += 1
+    return counter
+
+print(count_occurrences([-1, -1, 2], -1))
